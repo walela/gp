706887aa250e3297aad544e334caed177da20d0b
Austin Walela <austinwalela@gmail.com>
2025-04-11 16:45:16 +0300

    Fix header title

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -89,7 +89,6 @@ export default async function HomePage() {
   return (
     <div className="min-h-screen">
       <div className="container mx-auto px-2 py-4 space-y-8">
-
         <div>
           <h2 className="text-xl font-bold tracking-tight text-gray-700">Completed Tournaments</h2>
           <p className="text-muted-foreground mb-4">Completed tournaments in the 2025 Chess Kenya Grand Prix series</p>
